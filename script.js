21f686d73460eaa64debf209ac9659f22841e0d3
Diana Sepulveda <dcsepulvedar@gmail.com>
2022-04-14 18:48:24 -0500

    Adding aditional 3D models

--- script.js
+++ script.js
@@ -2,51 +2,86 @@ window.onload = () => {
     let places = staticLoadPlaces();
     renderPlaces(places);
 };
-
+//Función que contiene una matriz con las coordenadas en donde estarán los pokemon
 function staticLoadPlaces() {
    return [
        {
            name: 'Magnemite',
-           location: {
+           locationA: {
                lat: 4.753820,
                lng: -74.097179,
            }
        },
        {
-        name: 'Dragonite',
-        location: {
-            lat: 4.7509285,
-            lng: -74.0960931,
-        }
+            name: 'Dragonite',
+            locationB: {
+                lat: 4.7509285,
+                lng: -74.0960931,
+            }
     },
     {
-        name: 'Articuno',
-        location: {
-            lat: 4.7510015,
-            lng: -74.0960554,
-        }
+            name: 'Articuno',
+            locationC: {
+                lat: 4.754002,
+                lng: -74.097151,
+            }
     },
    ];
 }
+
+//Función para obtener los lugares y retornar los modelos en la escena
 
 function renderPlaces(places) {
    let scene = document.querySelector('a-scene');
 
    places.forEach((place) => {
-       let latitude = place.location.lat;
-       let longitude = place.location.lng;
+       let latitudeA = place.locationA.lat;
+       let longitudeA = place.locationA.lng;
+       let latitudeB = place.locationB.lat;
+       let longitudeB = place.locationB.lng;
+       let latitudeC = place.locationC.lat;
+       let longitudeC = place.locationC.lng;
 
-       let model = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
-       model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+       //Modelo A - Magnemite
+       let modelA = document.createElement('a-entity');
+       modelA.setAttribute('gps-entity-place', `latitude: ${latitudeA}; longitude: ${longitudeA};`);
+       modelA.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       modelA.setAttribute('rotation', '0 180 0');
+       modelA.setAttribute('animation-mixer', '');
+       modelA.setAttribute('scale', '0.5 0.5 0.5');
 
-       model.addEventListener('loaded', () => {
+       modelA.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
        });
 
-       scene.appendChild(model);
+       scene.appendChild(modelA);
+
+        //Modelo B - Dragonite
+        let modelB = document.createElement('b-entity');
+        modelB.setAttribute('gps-entity-place', `latitude: ${latitudeB}; longitude: ${longitudeB};`);
+        modelB.setAttribute('gltf-model', './assets/dragonite/scene.gltf');
+        modelB.setAttribute('rotation', '0 180 0');
+        modelB.setAttribute('animation-mixer', '');
+        modelB.setAttribute('scale', '0.5 0.5 0.5');
+ 
+        modelB.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+ 
+        scene.appendChild(modelB);
+
+        //Modelo C - Articuno
+        let modelC = document.createElement('c-entity');
+        modelC.setAttribute('gps-entity-place', `latitude: ${latitudeC}; longitude: ${longitudeC};`);
+        modelC.setAttribute('gltf-model', './assets/articuno/scene.gltf');
+        modelC.setAttribute('rotation', '0 180 0');
+        modelC.setAttribute('animation-mixer', '');
+        modelC.setAttribute('scale', '0.5 0.5 0.5');
+ 
+        modelC.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+ 
+        scene.appendChild(modelC);
    });
 }
